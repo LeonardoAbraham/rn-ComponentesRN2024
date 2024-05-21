21d28f3d6a6c043dad89a836c068372973406471
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-21 11:58:26 -0600

    Componente Switch

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,16 +1,47 @@
-import { View, Text } from 'react-native';
+import { Switch } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Card } from '../../components/ui/Card';
-import { Button } from '../../components/ui/Button';
+import { useState } from 'react';
+import { CustomSwitch } from '../../components/ui/CustomSwitch';
 
 export const SwitchScreen = () => {
+
+    //const [isEnabled, setIsEnabled] = useState(false);
+    //const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    const [state, setState] = useState({
+        isActive: true,
+        isHungry: false,
+        isHappy: true,
+    })
+
     return (
         <CustomView style={{ marginTop: 100, paddingHorizontal:10, }}>
             <Card>
-                <Button 
-                    text='Click me'
-                    onPress={() => {}}
+                <CustomSwitch 
+                    isOn={state.isActive}
+                    onChange={value => setState({...state, isActive: value})}
+                    text='¿Está activo?'
                 />
+
+                <CustomSwitch 
+                    isOn={state.isHungry}
+                    onChange={value => setState({...state, isHungry: value})}
+                    text='¿Tiene hambre?'
+                />
+
+                <CustomSwitch 
+                    isOn={state.isHappy}
+                    onChange={value => setState({...state, isHappy: value})}
+                    text='¿Es feliz?'
+                />
+                {/* <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={isEnabled ? '#08fd14' : '#f4f3f4'}
+                    ios_backgroundColor="#3e3e3e"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled}
+                /> */}
             </Card>
         </CustomView>
     );
