99f0b7679acb2b3c62f686bc76cd2f7d3f4aa196
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-22 16:22:48 -0600

    Pull to refresh

--- src/presentation/navigator/Navigator.tsx
+++ src/presentation/navigator/Navigator.tsx
@@ -5,6 +5,7 @@ import { Animation102Screen } from '../screens/animation/Animation102Screen';
 import { SwitchScreen } from '../screens/switches/SwitchScreen';
 import { AlertScreen } from '../screens/alerts/AlertScreen';
 import { TextInputScreen } from '../screens/inputs/TextInputScreen';
+import { PullToRefreshScreen } from '../screens/ui/PullToRefreshScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,9 +15,12 @@ export const Navigator = () => {
             <Stack.Screen name="HomeScreen" component={HomeScreen} />
             <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
             <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
+
             <Stack.Screen name="SwitchScreen" component={SwitchScreen} />
             <Stack.Screen name="AlertScreen" component={AlertScreen} />
             <Stack.Screen name="TextInputScreen" component={TextInputScreen} />
+
+            <Stack.Screen name="PullToRefreshScreen" component={PullToRefreshScreen} />
         </Stack.Navigator>
     );
 };
