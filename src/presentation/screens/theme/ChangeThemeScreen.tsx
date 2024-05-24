1e6e971400c0d9d8678d2d82aac454241d4c3919
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-24 10:21:34 -0600

    ThemeContext

--- src/presentation/screens/theme/ChangeThemeScreen.tsx
+++ src/presentation/screens/theme/ChangeThemeScreen.tsx
@@ -2,22 +2,27 @@ import { View } from 'react-native';
 import { CustomView } from '../../components/ui/CustomView';
 import { Title } from '../../components/ui/Title';
 import { Button } from '../../components/ui/Button';
+import { useContext } from 'react';
+import { ThemeContext } from '../../context/ThemeContext';
 
 export const ChangeThemeScreen = () => {
+
+    const {setTheme} = useContext(ThemeContext);
+
     return (
         <CustomView margin>
             <Title text='Cambiar tema' />
 
             <Button 
                 text='Light'
-                onPress={() => {}}
+                onPress={() => setTheme('light')}
             />
 
             <View style={{height: 10}} />
 
             <Button 
                 text='Dark'
-                onPress={() => {}}
+                onPress={() => setTheme('dark')}
             />
         </CustomView>
     );
