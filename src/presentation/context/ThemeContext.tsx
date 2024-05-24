a711b7ec9886582d158e3a177ec0f098f021d711
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-05-24 15:37:13 -0600

    Tema basado en el Sistema Operativo

--- src/presentation/context/ThemeContext.tsx
+++ src/presentation/context/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import { Children, createContext, PropsWithChildren, useState } from 'react';
+import { Children, createContext, PropsWithChildren, useEffect, useState } from 'react';
 import { darkColors, lightColors, ThemeColors } from "../../config/theme/theme";
+import { useColorScheme } from 'react-native';
 
 
 type ThemeColor = 'light' | 'dark';
@@ -16,7 +17,18 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: PropsWithChildren) => {
 
+    const colorScheme = useColorScheme();
+
     const [currentTheme, setCurrentTheme] = useState<ThemeColor>('light');
+
+    useEffect(() => {
+        if(colorScheme === 'dark'){
+            setCurrentTheme('dark');
+        }
+        else {
+            setCurrentTheme('light');
+        }
+    }, [colorScheme])
 
     const setTheme = (theme: ThemeColor) => {
         setCurrentTheme(theme)
